57363f33a57ff2a5b3c37fff28c515a4dd20217f
haru <2587655027@qq.com>
2024-07-29 19:35:53 +0800

    day-06-店铺浏览业务功能代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    void insert(User user);
+
+    /**
+     *  根据openID查询用户
+     * @param openId
+     * @return
+     */
+    @Select("SELECT * from user where openid = #{openId}")
+    User getByOpenId(String openId);
+}
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -43,4 +43,11 @@ public interface DishService {
     void updateWithFlavor(DishDTO dishDTO);
 
     List<Dish> findbyCategoryId(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
